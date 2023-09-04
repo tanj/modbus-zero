a95f781f9b0e2a8a262dc9470be61cbdaa0f88ef
John te Bokkel <tanj.tanj@gmail.com>
2023-09-04 08:10:45 -0700

    fix build for zig version update

--- build.zig
+++ build.zig
@@ -1,5 +1,15 @@
 const std = @import("std");
 
+const Test = struct {
+    name: []const u8,
+    path: []const u8,
+};
+
+const tests: []const Test = &.{
+    .{ .name = "main", .path = "src/main.zig" },
+    .{ .name = "modbus-app-types", .path = "src/mbap_types.zig" },
+};
+
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -23,19 +33,23 @@ pub fn build(b: *std.build.Builder) void {
     // running `zig build`).
     b.installArtifact(lib);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_main_tests = b.addRunArtifact(main_tests);
-
     // This creates a build step. It will be visible in the `zig build --help` menu,
     // and can be selected like this: `zig build test`
     // This will evaluate the `test` step rather than the default, which is "install".
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_main_tests.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    for (tests) |t| {
+        const tst = b.addTest(.{
+            .name = t.name,
+            .root_source_file = .{ .path = t.path },
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const run_main_tests = b.addRunArtifact(tst);
+
+        test_step.dependOn(&run_main_tests.step);
+    }
 }
